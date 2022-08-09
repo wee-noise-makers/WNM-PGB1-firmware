d52a0e0914e001a8b5e371c0fd235f7543d64367
Fabien Chouteau <fabien.chouteau@gmail.com>
2022-08-09 10:17:15 +0200

    Remove old BSPs and deprecated stuff

--- common/samd51/wnm-samd51-encoders.ads
+++
@@ -1,6 +0,0 @@
-package WNM.SAMD51.Encoders is
-
-   function Left return Integer;
-   function Right return Integer;
-
-end WNM.SAMD51.Encoders;
--- common/samd51/wnm-samd51-midi.ads
+++
@@ -1,7 +0,0 @@
-with WNM.Time;
-
-package WNM.Samd51.MIDI is
-
-   function Update return Time.Time_Ms;
-
-end WNM.Samd51.MIDI;
