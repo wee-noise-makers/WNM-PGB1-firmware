3713fc652d61bbfeb12d8dff26f32797b0191b02
Fabien Chouteau <fabien.chouteau@gmail.com>
2025-01-02 14:04:19 +0100

    Small hints for users when going in DFU mode

---
+++ common/gui/wnm-gui-dfu_mode_display.adb
@@ -0,0 +1,82 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                              Wee Noise Maker                              --
+--                                                                           --
+--                  Copyright (C) 2016-2024 Fabien Chouteau                  --
+--                                                                           --
+--    Wee Noise Maker is free software: you can redistribute it and/or       --
+--    modify it under the terms of the GNU General Public License as         --
+--    published by the Free Software Foundation, either version 3 of the     --
+--    License, or (at your option) any later version.                        --
+--                                                                           --
+--    Wee Noise Maker is distributed in the hope that it will be useful,     --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       --
+--    General Public License for more details.                               --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with We Noise Maker. If not, see <http://www.gnu.org/licenses/>. --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+--  with WNM.Screen;
+with WNM.LEDs;
+--  with WNM.GUI.Menu.Drawing; use WNM.GUI.Menu.Drawing;
+with WNM.Time;
+
+package body WNM.GUI.DFU_Mode_Display is
+
+   -----------
+   -- Setup --
+   -----------
+
+   procedure Setup is
+   begin
+
+      --  TODO: The DFU mode will reset all GPIO and therefore turn-off the
+      --  screen N-reset pin. Cannot find a way to keep the screen up at this
+      --  time...
+
+      --  --  Wait in case there is a running screen DMA transfer
+      --  WNM.Time.Delay_Milliseconds (30);
+      --
+      --  Screen.Clear;
+      --
+      --  Draw_Str_Center (0,  "Device in Update Mode");
+      --
+      --  Draw_Str_Center (12, "Go to");
+      --  Draw_Str_Center (20, "weenoisemakers.com");
+      --  Draw_Str_Center (28, "for new firmware and");
+      --  Draw_Str_Center (36, "sample transfer info");
+      --
+      --  Draw_Str_Center (49, "Turn off the device");
+      --  Draw_Str_Center (57, "to leave Update Mode");
+      --
+      --  Screen.Update;
+
+      LEDs.Turn_Off_All;
+      LEDs.Set_Hue (LEDs.Green);
+
+      --  U...
+      LEDs.Turn_On (B2);
+      LEDs.Turn_On (B10);
+      LEDs.Turn_On (B11);
+      LEDs.Turn_On (B12);
+      LEDs.Turn_On (B4);
+
+      --  P...
+      LEDs.Turn_On (WNM_Configuration.Menu);
+      LEDs.Turn_On (Pattern_Button);
+      LEDs.Turn_On (Song_Button);
+      LEDs.Turn_On (B6);
+      LEDs.Turn_On (B8);
+      LEDs.Turn_On (B7);
+      LEDs.Turn_On (B14);
+
+      LEDs.Update;
+
+      --  Wait for LEDs and screen DMA to complete
+      WNM.Time.Delay_Milliseconds (30);
+   end Setup;
+
+end WNM.GUI.DFU_Mode_Display;
--- common/gui/wnm-gui-dfu_mode_display.ads
+++ common/gui/wnm-gui-dfu_mode_display.ads
@@ -2,7 +2,7 @@
 --                                                                           --
 --                              Wee Noise Maker                              --
 --                                                                           --
---                  Copyright (C) 2016-2022 Fabien Chouteau                  --
+--                  Copyright (C) 2016-2024 Fabien Chouteau                  --
 --                                                                           --
 --    Wee Noise Maker is free software: you can redistribute it and/or       --
 --    modify it under the terms of the GNU General Public License as         --
@@ -19,46 +19,9 @@
 --                                                                           --
 -------------------------------------------------------------------------------
 
-with WNM.Persistent;
-with WNM_HAL;
-with WNM.Project.Library;
-with WNM.GUI.DFU_Mode_Display;
+package WNM.GUI.DFU_Mode_Display is
 
-package body WNM.Power_Control is
+   procedure Setup;
+   --  Setup LEDs and screen with DFU mode message
 
-   --------------------------
-   -- Save_Before_Shutdown --
-   --------------------------
-
-   procedure Save_Before_Shutdown is
-   begin
-      if WNM_HAL.Get_LFS_Config /= null then
-         Persistent.Save;
-         WNM.Project.Library.Try_Save_For_Shutdown;
-      end if;
-   end Save_Before_Shutdown;
-
-   ----------------
-   -- Power_Down --
-   ----------------
-
-   procedure Power_Down is
-   begin
-      Save_Before_Shutdown;
-      WNM_HAL.Power_Down;
-   end Power_Down;
-
-   --------------------
-   -- Enter_DFU_Mode --
-   --------------------
-
-   procedure Enter_DFU_Mode is
-   begin
-      Save_Before_Shutdown;
-
-      WNM.GUI.DFU_Mode_Display.Setup;
-
-      WNM_HAL.Enter_DFU_Mode;
-   end Enter_DFU_Mode;
-
-end WNM.Power_Control;
+end WNM.GUI.DFU_Mode_Display;
