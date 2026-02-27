bbcfa68c89d1f71ce57f09d3d61edc7dd1f8c14d
Fabien Chouteau <fabien.chouteau@gmail.com>
2026-02-27 06:56:23 +0100

    Check-in missing LFO icon

---
+++ common/generated/lfo_random.ads
@@ -0,0 +1,11 @@
+with WNM.Screen;
+
+package lfo_random is
+   pragma Style_Checks (Off);
+
+   Data : WNM.Screen.Bitmap := (W => 10, H => 10, Length_Byte => 13,
+Data => (
+ 1, 0, 0, 130, 132, 8, 1, 0, 4, 1, 128, 128, 128
+));
+
+end lfo_random;
